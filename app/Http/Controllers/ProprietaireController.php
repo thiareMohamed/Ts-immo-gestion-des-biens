c826824048d4c34522d2a204148df4dadd0039c5
thiaremohamed.mt@gmail.com <thiaremohamed.mt@gmail.com>
2022-04-05 10:45:45 +0000

    Add route and Controller to proprietaire

--- app/Http/Controllers/ProprietaireController.php
+++ app/Http/Controllers/ProprietaireController.php
@@ -2,11 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Proprietaire;
 use Illuminate\Http\Request;
 
 class ProprietaireController extends Controller
 {
     public function index(){
-        echo "hello";
+        $Proprietaires = Proprietaire::all();
+        return view('proprietaire/index', ['Proprietaires' => $Proprietaires]);
+    }
+
+    public function show($id){
+        $Proprietaire = Proprietaire::findOrFail($id);
+        return new JsonResponse($Proprietaire,200);
+    }
+
+    public function store(Request $request){
+        $new_Proprietaire = Proprietaire::create([
+            'nom'=>$request->nom,
+            'prenom'=>$request->prenom,
+            'civilite'=>$request->civilite,
+            'numero'=>$request->numero,
+            'sexe'=>$request->sexe,
+            'date_naissance'=>$request->date_naissance,
+            'lieu_naissance'=>$request->lieu_naissance,
+            'code_identite_national'=>$request->code_identite_national,
+            'numero_identite_national'=>$request->numero_identite_national
+
+        ]);
+        return new JsonResponse($new_Proprietaire,201);
+    }
+
+    public function update($id,Request $request){
+        $get_Proprietaire_to_update = Proprietaire::find($id);
+        $message = '';
+        $status = 0;
+        if (is_null($get_Proprietaire_to_update)){
+            $message = "L'idée que vous voulez modifier n'existe pas ";
+            $status = 404;
+        }else{
+            $get_Proprietaire_to_update->update([
+                'title'=>$request->title,
+                'description'=>$request->description,
+                'status'=>0
+            ]);
+            $message = $get_Proprietaire_to_update;
+            $status = 200;
+        }
+        return new JsonResponse($message,$status);
+    }
+
+    public function delete($id,Request $request){
+        $get_Proprietaire_to_delete = Proprietaire:: find($id);
+        $message = '';
+        $status = 0;
+        //testing if the Proprietaire is null
+        if (is_null($get_Proprietaire_to_delete)){
+            $message ="Cette idée n'existe pas";
+            $status = 404;
+        }else{
+        $title_Proprietaire = $get_Proprietaire_to_delete->title;
+        $message = "L'idée $title_Proprietaire a bien été suprimmé!!!";
+        $get_Proprietaire_to_delete->delete();
+        $status =200;
+        }
+        return new JsonResponse($message,$status);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProprietaireController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,18 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-    
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');    
+
+Route::middleware(['web'])->group(function () {
+   
+    Route::get('proprietaires',[ProprietaireController::class,'index']);
+    Route::get('proprietaires/{id}',[ProprietaireController::class,'show']);
+    Route::put('proprietaires/{id}',[ProprietaireController::class,'update']);
+    Route::delete('proprietaires/delete/{id}',[ProprietaireController::class,'delete']);
+    Route::post('proprietaires',[ProprietaireController::class,'store']);
+
+});
 
 require __DIR__.'/auth.php';
+
+// Route::get('proprietaires',[ProprietaireController::class,'index']);
